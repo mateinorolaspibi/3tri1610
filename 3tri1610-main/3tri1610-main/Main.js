d06d456a1474df51b9037312ae34619fd25cc5d7
mateinorolaspibi <127767299+mateinorolaspibi@users.noreply.github.com>
2023-10-23 12:22:18 -0300

    Add files via upload

---
+++ 3tri1610-main/3tri1610-main/Main.js
@@ -0,0 +1,46 @@
+function tocaSom(idElementoAudio){
+document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas=document.querySelectorAll('.tecla');
+ 
+ let contador=0;
+ //Estrtura de repetição - Enquanto 
+ while(contador < listaDeTeclas.length) {
+      const tecla=listaDeTeclas[contador]
+    const instrumento =tecla.classList[1];
+   const idAudio= '#som_${instrumento}';
+    console.log(instrumento);
+    tecla.onclick=function(){
+        tocaSom(idAudio);
+     };
+   contador = contador + 1;
+   console.log(contador);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 3tri1610-main/Main.js
+++ 3tri1610-main/Main.js
@@ -1,26 +1,28 @@
 function tocaSom(idElementoAudio){
-document.querySelector(idElementoAudio).play();
-}
-const listaDeTeclas=document.querySelectorAll('.tecla');
- 
- let contador=0;
- //Estrtura de repetição - Enquanto 
- while(contador < listaDeTeclas.length) {
-      const tecla=listaDeTeclas[contador]
-    const instrumento =tecla.classList[1];
-   const idAudio= '#som_${instrumento}';
-    console.log(instrumento);
-    tecla.onclick=function(){
-        tocaSom(idAudio);
-     };
-   contador = contador + 1;
-   console.log(contador);
-    }
+   document.querySelector(idElementoAudio).play();
+   }
+   const listaDeTeclas=document.querySelectorAll('.tecla');
+    
+    let contador=0;
+    //Estrtura de repetição - Enquanto 
+    while(contador < listaDeTeclas.length) {
+      const tecla = listaDeTeclas[contador];
+      const instrumento = tecla.classList[1];
+     const idAudio = '#som_ ${instrumento}';
+      console.log(instrumento);
+      console.log(idAudio);
 
-
-
-
-
+      tecla.onclick=function () {
+           tocaSom(idAudio);
+         };
+         tecla.onkeydown=function(){
+            tecla.classlist.add ('ativa');
+         }
+         tecla.onkeyup=function(){
+            tecla.classlist.remove('ative');
+         }
+      console.log(contador);
+       }
 
 
 
