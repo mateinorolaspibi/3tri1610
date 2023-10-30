d4733b368b7aeb4e35f78ffe244200ab067948f8
mateinorolaspibi <127767299+mateinorolaspibi@users.noreply.github.com>
2023-10-30 12:23:34 -0300

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,23 +1,32 @@
 function tocaSom(idElementoAudio){
-document.querySelector(idElementoAudio).play();
+ const elememto= document.querySelector(idElementoAudio);
+ if (elememto === null){
+  alert ('elemento não encontrado');
+  }
 }
-const listaDeTeclas=document.querySelectorAll('.tecla');
- 
- let contador=0;
- //Estrtura de repetição - Enquanto 
- while(contador < listaDeTeclas.length) {
-    const tecla=
-    const instrumento = listaDeTeclas[contador].classList[1];
+  const listaDeTeclas = document.querySelectorAll('.tecla');
+
+   for( let contador = 0; contador<listaDeTeclas.length; contador++){
+     const tecla = listaDeTeclas[contador];
+     const instrumento = tecla.classList[1];
+    const idAudio = `#som_ ${instrumento}`;
+
+     console.log(idAudio);
+
+     tecla.onclick = function(){
+          tocaSom(idAudio);
+        };
+      tecla.onkeydown = function(evento){
+        if (evento.code === 'Enter'||evento.code === 'Space'){
+          tecla.classList.add('ativa');
+        } 
+      };
+    tecla.onkeyup = function(){
+      tecla.classList.remove('ativa');
+    };
     console.log(instrumento);
-     listaDeTeclas[contador].onclick=function(){
-        tocaSom('#som_tecla_splash');
-     };
-   contador = contador + 1;
-   console.log(contador);
-    }
-
-
-
+    console.log(contador);
+        }
 
 
 
